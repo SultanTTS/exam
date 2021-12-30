61c9dd9b7429f0e32fc78b35d4da3c0efd13fd2b
SultanTTS <st11sultan@gmail.com>
2021-12-30 20:57:24 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import arithmetics as art
+
+print("Результат деления: ",art.division(3,14))
+
+print("Результат деления: ",art.addivision(2,21))
+
+print("Результат суммы: ",art.addition(,))
+
+print("Результат деления: ",art.division(5,13))
+
+print("Результат деления: ",art.division(1,39))
+
+print("Результат вычитания: ",art.subtraction(,))
+
+print("Результат умножения: ",art.multiplication(,))
+
+
